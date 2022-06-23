576450342533ef03843eeeb26f3ed7982f946377
Mann <mannshah24264@gmail.com>
2022-06-23 17:45:07 +0530

    babel an Webpack added in hangman

--- hangman/src/index.js
+++ hangman/src/index.js
@@ -1,9 +1,33 @@
-// import "./utilites";
+import Hangman from "./hangman";
+import getPuzzle from "./request";
 
-import otherSquare, { add, name } from "./utilites";
-import scream from "./scream";
+const puzzleEl = document.querySelector("#puzzle");
+const guessesEl = document.querySelector("#guesses");
+let game1;
 
-console.log("index.js");
-console.log(add(12, 25));
-console.log(scream(name));
-console.log(otherSquare(5));
+window.addEventListener("keypress", (e) => {
+  const guess = String.fromCharCode(e.charCode);
+  game1.makeGuess(guess);
+  render();
+});
+
+const render = () => {
+  puzzleEl.innerHTML = "";
+  guessesEl.textContent = game1.statusMessage;
+
+  game1.puzzle.split("").forEach((letter) => {
+    const letterEl = document.createElement("span");
+    letterEl.textContent = letter;
+    puzzleEl.appendChild(letterEl);
+  });
+};
+
+const startGame = async () => {
+  const puzzle = await getPuzzle("2");
+  game1 = new Hangman(puzzle, 5);
+  render();
+};
+
+document.querySelector("#reset").addEventListener("click", startGame);
+
+startGame();
--- hangman/src/utilites.js
+++
@@ -1,9 +0,0 @@
-const add = (a, b) => a + b;
-
-const name = "mann";
-
-const square = (x) => x * x;
-
-console.log("Hello");
-
-export { add, name, square as default };
