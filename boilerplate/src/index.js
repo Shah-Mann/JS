b8ae239cc08328a352d54abb9a2d4ac7835b9a99
Mann <mannshah24264@gmail.com>
2022-06-23 16:47:31 +0530

    babel an Webpack added

---
+++ boilerplate/src/index.js
@@ -0,0 +1,8 @@
+// import "./utilites";
+
+import { add, name } from "./utilites";
+import { scream } from "./scream";
+
+console.log("index.js");
+console.log(add(12, 25));
+console.log(scream(name));
