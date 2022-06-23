c4691f6c77394b0d554874963e1b4490af2ecdbe
Mann <mannshah24264@gmail.com>
2022-06-23 16:15:33 +0530

    Babel and webpacked added

---
+++ boilerplate/src/index.js
@@ -0,0 +1,11 @@
+const name = "Mann Shah";
+console.log(name);
+
+class Hangman {
+  myMethods() {
+    return "testing";
+  }
+}
+
+const hangman = new Hangman();
+console.log(hangman.myMethods());
