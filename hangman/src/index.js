f659b2e79e6651b3a673c3f043e71bf419757e6e
Mann <mannshah24264@gmail.com>
2022-06-23 17:35:28 +0530

    folder name changes

---
+++ hangman/src/index.js
@@ -0,0 +1,9 @@
+// import "./utilites";
+
+import otherSquare, { add, name } from "./utilites";
+import scream from "./scream";
+
+console.log("index.js");
+console.log(add(12, 25));
+console.log(scream(name));
+console.log(otherSquare(5));
---
+++ hangman/src/utilites.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+
+const name = "mann";
+
+const square = (x) => x * x;
+
+console.log("Hello");
+
+export { add, name, square as default };
