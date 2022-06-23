e08961bfc5a97ffdc726bab278b550814dce3f3d
Mann <mannshah24264@gmail.com>
2022-06-23 16:51:51 +0530

    babel an Webpack added

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,8 +1,9 @@
 // import "./utilites";
 
-import { add, name } from "./utilites";
+import otherSquare, { add, name } from "./utilites";
 import { scream } from "./scream";
 
 console.log("index.js");
 console.log(add(12, 25));
 console.log(scream(name));
+console.log(otherSquare(10));
--- boilerplate/src/utilites.js
+++ boilerplate/src/utilites.js
@@ -1,5 +1,6 @@
-console.log("utilites.js");
-
 export const add = (a, b) => a + b;
 
 export const name = "mann";
+
+const square = (x) => x * x;
+export default square;
