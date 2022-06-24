715e155db01d30c4722e86aa4c47d60cfce04083
Mann <mannshah24264@gmail.com>
2022-06-24 17:34:53 +0530

    todo-app completed with babel and webpacket

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,16 +1,46 @@
-// Set up index.html to load the bundle
-// Make sure to load uuid via an npm module when necessary
+import { getFilters, setFilters } from "./filters";
+import {
+  getTodos,
+  createTodo,
+  removeTodo,
+  toggleTodo,
+  loadTodos,
+} from "./todos";
+import { generateTodoDOM, generateSummaryDOM, renderTodos } from "./views";
 
-// --
+renderTodos();
 
-// Add necessary imports
+document.querySelector("#i2").addEventListener("input", function(e) {
+  setFilters({
+    searchText: e.target.value,
+  });
+  renderTodos();
+});
 
-// Render initial todos
+document.querySelector("#new-todo").addEventListener("submit", function(e) {
+  setFilters({
+    searchText: e.target.value,
+  });
+  const text = e.target.elements.text.value.trim();
+  e.preventDefault();
 
-// Set up search text handler
+  if (text.length > 0) {
+    createTodo(text);
+    renderTodos();
+    e.target.elements.text.value = "";
+  }
+});
 
-// Set up checkbox handler
+document.querySelector("#hide-completed").addEventListener("change", (e) => {
+  setFilters({
+    hideCompleted: e.target.checked,
+  });
+  renderTodos();
+});
 
-// Set up form submission handler
-
-// Bonus: Add a watcher for local storage
+window.addEventListener("storage", (e) => {
+  if (e.key === "todos") {
+    loadTodos();
+    renderTodos();
+  }
+});
