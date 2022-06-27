bc003d5ddadf54a412297e3fb020a8c506744397
Mann <mannshah24264@gmail.com>
2022-06-27 14:58:24 +0530

    Some extra codes

---
+++ Problems Solving/even.js
@@ -0,0 +1,41 @@
+// function isPrime(num) {
+//   for (var i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function display(n) {
+//   var arr = [2];
+//   for (var i = 3; i < n; i += 2) {
+//     if (isPrime(i)) {
+//       arr.push(i);
+//     }
+//   }
+//   console.log(arr); // use arr result on your own
+// }
+
+// display(100);
+
+// function isEven(num) {
+//   for (var i = 0; i < num; i++) {
+//     if (num % 2 === 0) {
+//       return true;
+//     }
+//   }
+//   // return false;
+// }
+// console.log(`The odd number are as follow`);
+// function display(n) {
+//   var arr = [];
+//   for (var i = 1; i < n; i++) {
+//     if (isEven(i)) {
+//       arr.push(i);
+//     }
+//   }
+//   console.log(arr);
+// }
+
+// display(100);
