7a7247a0f73b69e1b0cde12ddaaed836b158ab78
Mann Shah <86416855+Shah-Mann@users.noreply.github.com>
2022-12-27 15:20:33 +0530

    little chance

--- hangman/src/request.js
+++ hangman/src/request.js
@@ -11,33 +11,33 @@ const getPuzzle = async (wordCount) => {
   }
 };
 
-const getCurrentCountry = async () => {
-  const location = await getLocation();
-  return getCountry(location.country);
-};
+// const getCurrentCountry = async () => {
+//   const location = await getLocation();
+//   return getCountry(location.country);
+// };
 
-const getCountry = async (countryCode) => {
-  const response = await fetch("//restcountries.com/v3.1/all");
+// const getCountry = async (countryCode) => {
+//   const response = await fetch("//restcountries.com/v3.1/all");
 
-  if (response.status === 200) {
-    const data = await response.json();
-    return data.find((country) => country.cca2 === countryCode);
-  } else {
-    throw new Error("Unable to fetch the country");
-  }
-};
+//   if (response.status === 200) {
+//     const data = await response.json();
+//     return data.find((country) => country.cca2 === countryCode);
+//   } else {
+//     throw new Error("Unable to fetch the country");
+//   }
+// };
 
-const getLocation = async () => {
-  const response = await fetch("//ipinfo.io/json?token=8efab4cc9f1acc");
+// const getLocation = async () => {
+//   const response = await fetch("//ipinfo.io/json?token=8efab4cc9f1acc");
 
-  if (response.status === 200) {
-    return response.json();
-  } else {
-    throw new Error("Unable to get the current location");
-  }
-};
+//   if (response.status === 200) {
+//     return response.json();
+//   } else {
+//     throw new Error("Unable to get the current location");
+//   }
+// };
 
-export { getPuzzle as default };
+// export { getPuzzle as default };
 // const getCountryold = (countryCode) => {
 //   return fetch("https://restcountries.com/v3.1/all")
 //     .then((response) => {
